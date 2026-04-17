32dd1da70d81b06b07d5689f03fd519b4e2529d5
mirthh00 <mirthh00@gmail.com>
2026-04-17 15:16:30 +0200

    Update page.js

--- src/app/success/page.js
+++ src/app/success/page.js
@@ -2,7 +2,8 @@ import Link from "next/link";
 import { prisma } from "@/lib/prisma";
 
 export default async function PaymentSuccessPage({ searchParams }) {
-  const leadId = searchParams.leadId;
+  const params = await searchParams;
+  const leadId = params.leadId;
 
   let trackingCode = null;
 
